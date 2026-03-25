4d2a6ddf58a2fffa22148cff8256ef74d79e7a70
Ashi Gupta <ashi2004gupta@gmail.com>
2026-03-26 02:41:00 +0530

    Center tempo popup for consistent modal behavior

--- src/app/components/tempo-selector/tempo-selector.component.ts
+++ src/app/components/tempo-selector/tempo-selector.component.ts
@@ -30,7 +30,6 @@ import { MAXTEMPO, MINTEMPO } from 'src/app/constants';
         <span id="tempo">{{ tempo }} bpm</span>
     </h1>
   </div>
-  
   </div>
   `,
   styles: [`
@@ -39,15 +38,16 @@ import { MAXTEMPO, MINTEMPO } from 'src/app/constants';
     text-align: center;
     margin:10px 10px 10px 10px;
   }
-.title-section-wrapper{
-font-size: 1em;
-}
+  .title-section-wrapper{
+    font-size: 1em;
+  }
   .tempo {
     flex-grow: 1;
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100%; /* Fill container */
+    cursor: pointer;
   }
   .simulate-input {
     background-color: #fff;
